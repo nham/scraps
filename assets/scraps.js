32d4ebc831aa4d3d8cce1b59880f5730e705ff76
Nick Hamann <hamann.nick@gmail.com>
2011-03-07 13:25:14 -0600

    underscore.js apparently doesnt support strings with indexOf, so I'm just using the ECMAScript 5 indexOf, which works for every browser except IE 8 and below

--- assets/scraps.js
+++ assets/scraps.js
@@ -165,17 +165,9 @@ $(function(){
       if(searchstr.length > 1) {
         $("#scrap-list").children().each(function() {
           var scrap_el = $(this);
-          console.log('search string: '+searchstr);
-          console.log('element text: '+scrap_el.text());
-
-          console.log(_.indexOf("chahadabbe", "had"));
-          console.log(_.indexOf("chahadabbe", "ha"));
-
-          if(_.indexOf(scrap_el.text(), searchstr) === -1) {
-            console.log('we hidin\n');
+          if(scrap_el.text().indexOf(searchstr) === -1) {
             scrap_el.hide();
 	  } else {
-            console.log('we showin\n');
             scrap_el.show();
           }
         });
