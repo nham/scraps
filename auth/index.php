0768b37316aa3ff8baa0d1416f6a5e6ce192df53
Nick Hamann <hamann.nick@gmail.com>
2011-03-08 21:36:45 -0600

    Added code for logging in (setting a cookie). Now to make it do something...

--- auth/index.php
+++ auth/index.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head><title>set scraps password</title></head>
-<body>
-
-
 <?php
 
 require('phpass-0.3/PasswordHash.php');
 
+function db_connect() {
+  return new SQLite3('../db/scraps.db');
+}
 
 class Password {
   var $pw;
   var $hasher;
+  var $db;
 
-  function __construct($db) {
-    $res = $db->querySingle('SELECT pw FROM password');
+  function __construct() {
+    $this->db = db_connect();
+    $res = $this->db->querySingle('SELECT pw FROM password');
     
     if($res === false)
       throw new Exception("Couldn't select password from database");
@@ -24,7 +23,7 @@ class Password {
     $this->hasher = new PasswordHash(8, false);
   }
 
-  function passExists() {
+  function exists() {
     return ($this->pw != "");
   }
 
@@ -32,72 +31,164 @@ class Password {
     return $this->hasher->CheckPassword($entered, $this->pw);
   }
 
-  function setPassword($new, $db) {
+  function set($new, $db) {
     $hashed_pw = $this->hasher->HashPassword($new);
-    if(!$db->exec("UPDATE password SET pw='$hashed_pw'"))
+    if(!$this->db->exec("UPDATE password SET pw='$hashed_pw'"))
       throw new Exception("Couldn't set the password in the database.");
     else
       return true;
   }
 }
 
+class Auth {
+  var $db;
+  var $anums;
 
-try {
-  $db = new SQLite3('../db/scraps.db');
-  $P = new Password($db);
-  $pass_exists = $P->passExists();
-} catch(Exception $e) {
-  die($e->getMessage());
-}
+  function __construct() {
+    $this->db = db_connect();
 
-if($_POST['newpass']) {
-
-  if($pass_exists && !$P->isValid($_POST['password'])) {
-    $pass_incorrect = true;
-  } else {
-    $confirm_failed = $_POST['newpass'] != $_POST['passconfirm'];
-
-    // (Try to) set the password
-    if(!$confirm_failed) {
-      try {
-        $set_pass = $P->setPassword($_POST['newpass'], $db);
-      } catch(Exception $e) {
-        die($e->getMessage());
-      }
-    }
-
+    $this->anums = array();
+    $res = $this->db->query('SELECT anum FROM auth_nums');
+    while($row = $res->fetchArray())
+      array_push($this->anums, $row['anum']);
   }
 
+  function create($num) {
+    $this->db->exec("INSERT INTO auth_nums VALUES ('$num')");
+  }
+
+  function isValid($num) {
+    foreach($this->anums as $anum)
+      if($anum == $num)
+        return true;
+
+    return false;
+  }
 }
 
+
+function setpw() {
+
+  try {
+    $P = new Password();
+    $pass_exists = $P->exists();
+  } catch(Exception $e) {
+    die($e->getMessage());
+  }
+
+  if($_POST['newpass']) {
+
+    if($pass_exists && !$P->isValid($_POST['password'])) {
+      $pass_incorrect = true;
+    } else {
+      $confirm_failed = $_POST['newpass'] != $_POST['passconfirm'];
+
+      // (Try to) set the password
+      if(!$confirm_failed) {
+        try {
+          $set_pass = $P->set($_POST['newpass'], $db);
+        } catch(Exception $e) {
+          die($e->getMessage());
+        }
+      }
+    }
+  }
+  ?>
+
+  <!DOCTYPE html>
+  <html>
+  <head><title>set scraps password</title></head>
+  <body>
+
+  <?php if($pass_incorrect): ?>
+
+    <p>The password entered does not match the current password.</p>
+
+  <?php elseif($set_pass): ?>
+
+    <p>Password set successfully.</p>
+
+  <?php else: ?>
+
+    <?php if($confirm_failed): ?>
+    <p>The passwords you entered don't match.</p>
+    <?php endif; ?>
+
+    <form method="post">
+
+    <?php if($pass_exists): ?>
+    <input name="password" type="password" placeholder="Current password?" \>
+    <?php endif; ?>
+
+    <input name="newpass" type="password" placeholder="New password"\>
+    <input name="passconfirm" type="password" placeholder="Confirm new password" \>
+    <input type="submit" value="Set Password" />
+    </form>
+
+  <?php endif;
+}
+
+
+function login() {
+  $A = new Auth();
+
+  if($_POST['password']) {
+    $P = new Password();
+
+    if(!$P->isValid($_POST['password'])) {
+      $pass_incorrect = true;
+    } else {
+      $set_cookie = true;
+
+      setcookie("auth", "", time() - 3600);
+
+      $rand1 = base_convert(mt_rand(), 10, 36);
+      $rand2 = base_convert(mt_rand(), 10, 36);
+      $cookieval = $rand1.$rand2;
+
+      setcookie("auth", $cookieval, time()+3600*24*365, '/scraps/');
+      $A->create($cookieval);
+    }
+  } else {
+    if(isset($_COOKIE['auth']) && $A->isValid($_COOKIE['auth']))
+      $already_set = true;
+  }
+  ?>
+
+  <!DOCTYPE html>
+  <html>
+  <head><title>set scraps password</title></head>
+  <body>
+
+  <?php if($pass_incorrect): ?>
+
+    <p>The password entered does not match the current password.</p>
+
+  <?php elseif($set_cookie): ?>
+
+    <p>Y'all should be logged in now.</p>
+
+  <?php elseif($already_set): ?>
+
+    <p>Y'all is already logged in.</p>
+
+  <?php else: ?>
+
+    <form method="post">
+    <input name="password" type="password" placeholder="Password?" \>
+    <input type="submit" value="Login" />
+    </form>
+
+  <?php endif;
+}
+
+
+if(isset($_GET['a']))
+  switch($_GET['a']) {
+    case "setpw": setpw(); break;
+    case "login": login(); break;
+  }
 ?>
-
-<?php if($pass_incorrect): ?>
-
-  <p>The password entered does not match the current password.</p>
-
-<?php elseif($set_pass): ?>
-
-  <p>Password set successfully.</p>
-
-<?php else: ?>
-
-  <?php if($confirm_failed): ?>
-  <p>The passwords you entered don't match.</p>
-  <?php endif; ?>
-
-  <form method="post">
-
-  <?php if($pass_exists): ?>
-  <input name="password" type="password" placeholder="Current password" \>
-  <?php endif; ?>
-
-  <input name="newpass" type="password" placeholder="New password"\>
-  <input name="passconfirm" type="password" placeholder="Confirm new password" \>
-  <input type="submit" value="Set Password" />
-  </form>
-
-<?php endif; ?>
 
 </body>
 </html>
--- db/dbinit.php
+++ db/dbinit.php
@@ -5,6 +5,8 @@ $db = new SQLite3('scraps.db');
 $db->exec('CREATE TABLE scraps (id integer PRIMARY KEY, created integer, body text)');
 
 $db->exec('CREATE TABLE password (pw text)');
-$db->exec('INSERT INTO password VALUES ("")');
+$db->exec('INSERT INTO password VALUES ("")')
+
+$db->exec('CREATE TABLE auth_nums (anum text)');
 
 ?>
