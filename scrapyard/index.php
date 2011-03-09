790eca0af260719c17d1fcee91cadb4de0bd2739
Nick Hamann <hamann.nick@gmail.com>
2011-03-09 16:54:13 -0600

    Refactored scrapyard/index.php. Much prettier now.

--- scrapyard/index.php
+++ scrapyard/index.php
@@ -6,49 +6,68 @@ $path_to_scraps = '/scraps';
 $db = new SQLite3('../db/scraps.db');
 $id = (int) str_replace($path_to_scraps."/scrapyard/", "", $_SERVER['REQUEST_URI']);
 
+
+function serve_all_scraps() {
+  global $db;
+
+  $scraps = array();
+  $res = $db->query('SELECT * FROM scraps');
+
+  while($row = $res->fetchArray(SQLITE3_ASSOC))
+    array_push($scraps, $row);
+
+  return json_encode($scraps);
+}
+
+
+function serve_scrap($id) {
+  global $db;
+  $res = $db->query("SELECT * FROM scraps WHERE id=$id");
+  return json_encode($res->fetchArray(SQLITE3_ASSOC));
+}
+
+
+function save_scrap($scrap) {
+  global $db;
+  
+  $stmt = $db->prepare('INSERT INTO scraps (created, body) VALUES (:created, :body)');
+  $stmt->bindValue(':created', $scrap['created'], SQLITE3_INTEGER);
+  $stmt->bindValue(':body', $scrap['body'], SQLITE3_TEXT);
+  $stmt->execute();
+
+  $scrap['id'] = $db->lastInsertRowID();
+  return json_encode($scrap);
+}
+
+
+function modify_scrap($scrap, $id) {
+  global $db;
+  $stmt = $db->prepare('UPDATE scraps SET body=:body WHERE id=:id');
+  $stmt->bindValue(':body', $scrap['body'], SQLITE3_TEXT);
+  $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+  $stmt->execute();
+}
+
+function delete_scrap($id) {
+  global $db;
+  $db->exec("DELETE FROM scraps WHERE id=$id");
+}
+
+
+// Handle the request
 switch($_SERVER['REQUEST_METHOD']) {
   case "GET":
-    if(empty($id)) {
-      $result = $db->query('SELECT * FROM scraps');
-
-      echo "[";
-      echo json_encode($result->fetchArray(SQLITE3_ASSOC));
-      while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        echo "," . json_encode($row);
-      }
-      echo "]";
-
-    } else {
-      $result = $db->query("SELECT * FROM scraps WHERE id=$id");
-      echo json_encode($result->fetchArray(SQLITE3_ASSOC));
-    }
-
+    echo (empty($id)) ? serve_all_scraps() : serve_scrap($id);
     break;
 
   case "POST":
     $scrap = json_decode(stripslashes($_POST['model']), true);
 
-    if(!isset($_POST['_method'])) {
-      $stmt = $db->prepare('INSERT INTO scraps (created, body) VALUES (:created, :body)');
-      $stmt->bindValue(':created', $scrap['created'], SQLITE3_INTEGER);
-      $stmt->bindValue(':body', $scrap['body'], SQLITE3_TEXT);
-      $stmt->execute();
-
-      $scrap['id'] = $db->lastInsertRowID();
-      echo json_encode($scrap);
-    } else {
-      switch($_POST['_method']) {
-        case "PUT":
-          $stmt = $db->prepare('UPDATE scraps SET body=:body WHERE id=:id');
-          $stmt->bindValue(':body', $scrap['body'], SQLITE3_TEXT);
-          $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
-          $stmt->execute();
-          break;
-
-        case "DELETE":
-          $db->exec("DELETE FROM scraps WHERE id=$id");
-          break;
-      }
-    }
-    break;     
+    // PUT and DELETE are tunnelled over POST
+    if(!isset($_POST['_method']))
+      echo save_scrap($scrap);
+    else if($_POST['_method'] == "PUT")
+      modify_scrap($scrap, $id);
+    else if($_POST['_method'] == "DELETE")
+      delete_scrap($id);
 }
